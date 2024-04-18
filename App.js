832888a8cfe116888ec0532287257b6abeea384b
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-04-18 03:45:28 -0400

    start

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
-console.log("Test!");
+import Hello from "./Hello.js";
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+Hello(app);
+app.listen(4000);
+
