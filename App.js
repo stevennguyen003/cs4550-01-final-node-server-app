e17b6b72f9310ae0f9f138b873d54e1af08179c8
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-04-18 04:16:04 -0400

    mongo

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 import Hello from "./Hello.js";
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+import session from "express-session";
+mongoose.connect("mongodb://127.0.0.1:27017/senzu");
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+}));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
 app.use(express.json());
+app.use(
+    session(sessionOptions)
+);
 Hello(app);
 app.listen(process.env.PORT || 4000);
 
